9c9e871c7cfd606a2f67845d935f32a8c12adcef
Tiller <ctiller@proforma.com>
2018-05-11 10:13:29 -0400

    Add font awesome and adjust recent work backbar

--- public/main.js
+++ public/main.js
@@ -1,1 +1,15 @@
+window.onload = () => {
+    init();
+}
+
+const init = () => {
+    const topNavBar = document.querySelector(".navbar-top");
+    const mobileNav = document.querySelector(".navigation");
+
+    topNavBar.addEventListener("click" , (e) => {
+        console.log("I've been clicked!");
+        mobileNav.classList.toggle("nav-mobile-hidden");
+    })
+}
+
 
